3d45e7dcefd6b2e1916ac06be2858b04de780581
Hery Martin <scorpionxii@gmail.com>
2017-02-13 15:00:58 -0500

    Saving work

--- mars_rover/rover.js
+++ mars_rover/rover.js
@@ -1,25 +1,85 @@
+var landMap = [
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
 var myRover = {
-  position: [0,0], 
-  direction: 'N'
+  positionX: 0,
+  positionY: 0, 
+  	compass: ['N','E','S','W']
 };
 
+function rotateDirection(direction, array){
+  if(direction == "left")
+    array.unshift(array.pop());
+  else if (direction == "right")
+    array.push(array.shift());
+  return array;
+}
+
+function isClear(valX , valY) {
+	if (valX > landMap.length - 1 || valY > landMap[0].length - 1) {
+		document.write("Position is out of bounds");
+		return false;
+	}
+	
+	if (landMap[valX][valY] == 0) {
+		writeLn("Next position it's clear");
+		return true;	
+	}
+	
+	if (landMap[valX][valY] == 1) {
+		document.write("There is an obstacle");
+		return false;	
+	}
+}
+
 function goForward(rover) {
-  switch(rover.direction) {
+  switch(rover.compass[0]) {
     case 'N':
-      rover.position[0]++;
+      rover.positionX++;
       break;
     case 'E':
-      rover.position[1]++;
+      rover.positionY++;
       break;
     case 'S':
-      rover.position[0]--;
+      rover.positionX--;
       break;
     case 'W':
-      rover.position[1]--;
+      rover.positionY--;
       break;
   };
 
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
+  console.log("New Rover Position: [" + rover.positionX + ", " + rover.positionY + "]");
+  
+}
+
+function writeLn(string) {
+	document.write(string);
+	document.write("<br />");
 }
 
 goForward(myRover);
+writeLn(myRover.compass[0]);
+writeLn(myRover.compass);
+
+rotateDirection("left", myRover.compass);
+writeLn(myRover.compass[0]);
+writeLn(myRover.compass);
+
+rotateDirection("right", myRover.compass);
+writeLn(myRover.compass[0]);
+writeLn(myRover.compass);
+
+writeLn(landMap[0].length);
+
+writeLn(landMap[1][1]);
+isClear(10,1);
